c40c4ef7f5b48d16dd37829591d29d0532757c1b
techie_joe <kelechijoseph985@gmail.com>
2025-06-02 14:09:51 +0100

    deployment 8

--- src/utils/strategies/jwt.ts
+++ src/utils/strategies/jwt.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { Request } from 'express';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(configService: ConfigService) {
     super({
-      jwtFromRequest: ExtractJwt.fromExtractors([
-        (req: Request) => req?.cookies?.jwt || null,
-      ]),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET'),
+      secretOrKey: configService.get<string>('JWT_SECRET', { infer: true }),
     });
   }
 
@@ -22,4 +19,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       name: payload.name,
     };
   }
+
 }
+
